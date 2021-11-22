fcf47a45255faf95694f36571b7d603fb8a88820
Fu Diwei <fudiwei@sina.com>
2021-11-22 16:27:58 +0800

    feat: 新增 __wxConfig 的全局声明

---
+++ types/wx.miniprogram-api-extensions.d.ts
@@ -0,0 +1,59 @@
+declare const __wxConfig: {
+    wxAppInfo?: {
+        maxDownloadConcurrent: number;
+        maxRequestConcurrent: number;
+        maxUploadConcurrent: number;
+        maxWebsocketConnect: number;
+        maxWorkerConcurrent: number;
+    };
+
+    accountInfo?: {
+        appId: string;
+        icon: string;
+        nickname: string;
+    };
+
+    appLaunchInfo?: {
+        path: string;
+        query: Record<string, string>;
+        referrerInfo?: Record<string, string>;
+        scene: number;
+        shareTicket?: string;
+    };
+
+    appType: number;
+
+    debug?: boolean;
+
+    ext?: Record<string, any>;
+
+    extAppid?: string;
+
+    platform: string;
+
+    envVersion: 'develop' | 'trial' | 'release';
+
+    entryPagePath: string;
+
+    pages: string[];
+
+    networkTimeout: {
+        connectSocket: number;
+        downloadFile: number;
+        request: number;
+        uploadFile: number;
+    };
+
+    tabBar?: {
+        borderStyle?: 'black' | 'white';
+        color?: string;
+        backgroundColor?: string;
+        selectedColor?: string;
+        list: Array<{
+            pagePath: string;
+            iconPath: string;
+            selectedIconPath: string;
+            text: string;
+        }>;
+    };
+};
